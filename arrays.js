bc5171c81c447dff68d89985e9a91c2f242a9821
Alex East <alexeastltd@gmail.com>
2019-05-15 22:00:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
-funcion removeElementFromEndOfArray(array) {
+function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
