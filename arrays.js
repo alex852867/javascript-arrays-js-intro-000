ae69d09b3b9e713d2e14fabb75e21d0e71165c2e
Alex East <alexeastltd@gmail.com>
2019-05-15 21:25:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,14 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, element){
-  return ['element', ...array]
+function addElementToBeginningOfArray(array, foo){
+  return ['foo', ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array;
 }
-function addElementToEndOfArray(array, element) {
-  return [...array, 'element']
+function addElementToEndOfArray(array, foo) {
+  return [...array, 'foo']
 }
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
