49ba3f9ad455f02af238e49e36234dfb7a9d2708
Alex East <alexeastltd@gmail.com>
2019-05-15 21:41:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array
 }
-function accessElementInArray(array) {
-    return array[1];
+function accessElementInArray(array, index) {
+    return array[index];
 }
