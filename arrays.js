f8dfca7125f4ffead0b2396c27440fb7db4d31f4
Alex East <alexeastltd@gmail.com>
2019-05-15 21:10:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
   return ['element', ...array]
 }
-function destructivelyAddElementToBeginningOfArray() {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array;
 }
