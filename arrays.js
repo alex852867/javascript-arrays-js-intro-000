1b21a0fb893f68725dd46938b28df21f2610e72e
Alex East <alexeastltd@gmail.com>
2019-05-15 20:54:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray('Creme Egg'){
-  return ['Creme Egg' ...chocolate,]
+  return ['Creme Egg' ...chocolateBars,]
 }
