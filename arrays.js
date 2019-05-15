fcfff2aa0b5c30687c688da7ef7aa40b51999fb5
Alex East <alexeastltd@gmail.com>
2019-05-15 21:01:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(){
   return ['Creme Egg', ...chocolateBars,]
 }
+function destructivelyAddElementToBeginningOfArray() {
+  return chocolateBars.unshift('Mars Bar')
+}
